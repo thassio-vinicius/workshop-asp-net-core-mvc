891c926d536a0e25b84f23a22f20f9c6f0f0b020
thassio-vinicius <thassio.vinicius.costa@gmail.com>
2019-10-10 01:27:09 -0300

    Deleting views and controllers

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SalesWebMVC.Models
+﻿namespace SalesWebMVC.Models
 {
     public class Department
     {
